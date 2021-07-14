3d9062ab10e447b8a0f3879b78515c636c0999d0
ha271097 <nguyenquangha2710@gmail.com>
2021-07-14 22:33:22 +0700

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'test-snt';
+ public checkSnt(snt:number) : boolean{
+  
+    if(snt == 2 || snt == 3){
+      return true;
+    }
+    if(snt <= -1){
+      return false;
+    } 
+    for (let i = 2; i < Math.sqrt(snt); i++){
+      if(snt % i == 0){
+        return false;
+      }
+    }
+    return true;
+  }
+  public sumSnt(): number{
+    let array = [1, 5, 9, 2, 6, 15, 19, 35, 51, 53];
+    let sum = 0;
+    for (let  i = 0 ; i < array.length ; i ++){
+      if(this.checkSnt(array[i])){
+        sum += array[i];
+      }
+    }
+    console.log(sum)
+    return sum;
+  }
+  total : number = this.sumSnt();
+
 }
